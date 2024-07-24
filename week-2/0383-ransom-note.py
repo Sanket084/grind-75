9214c265855f5f55e5b2234d8b964d8716554281
Sanket084 <kadlag.sanket82@gmail.com>
2024-07-25 01:51:53 +0530

    Add Week 2 Problems

---
+++ week-2/0383-ransom-note.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        return (Counter(ransomNote) - Counter(magazine)) == {}
+        dict = {}
+
+        for char in magazine:
+            if char not in dict:
+                dict[char] = 1
+            else:
+                dict[char] -= 1
+        
+        for char in ransomNote:
+            if char in dict and dict[char] > 0:
+                dict[char] -= 1
+            else:
+                return False
+        return True
